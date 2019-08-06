58fc062838e9fb1dd622f2f3a65eb14e391b39c7
Alex <agordon1123@gmail.com>
2019-08-06 16:15:08 -0400

    data retreived from api

--- github-usercard/src/App.js
+++ github-usercard/src/App.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import './App.css';
+import Axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>GitHub Usercard</h1>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      data: {}
+    }
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData = () => {
+    Axios('https://api.github.com/users/agordon1123')
+      .then(res => {
+        this.setState(res.data)
+        console.log('res.data ', res.data)
+        console.log('this.state ', this.state)
+      })
+      .catch(err => {
+        console.log('err ', err)
+      })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>GitHub Usercard</h1>
+      </div>
+    );
+  
+  }
 }
 
 export default App;
